ee9e59d56031f9c7607179a7a8d466eb6c2ae8b4
root <root@localhost.localdomain>
2015-06-17 19:19:11 +0200

    big fix for Fedora, but HDF5/harminv/.. support missing

--- python-meep-install.sh
+++ python-meep-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+j!/bin/bash
 ## This is a compilation procedure that worked for me to setup the python-meep 
 ## with some utilities on a Debian-based system.
 
@@ -6,32 +6,62 @@
 MPI="openmpi"
 #MPI="mpich"
 #MPI="mpich2"
-#MPI="serial"			## i.e. no multiprocessing used
+#MPI="serial"		## i.e. no multiprocessing used
 
+## --- Preparation and build dependencies -------------------------------------
 if [ "$MPI" = "openmpi" ] || [ "$MPI" = "mpich" ] || [ "$MPI" = "mpich2" ] ; then meep_opt="--with-mpi"; fi
 
-## --- Build dependencies -----------------------------------------------------
-## (list obtained from https://launchpad.net/ubuntu/quantal/+source/meep)
-sudo apt-get update
-sudo apt-get install -y autotools-dev autoconf chrpath debhelper gfortran \
-    g++ git guile-2.0-dev h5utils imagemagick libatlas-base-dev libfftw3-dev libgsl0-dev \
-    libharminv-dev  liblapack-dev libtool pkg-config swig  zlib1g-dev
+if [ -d /etc/apt ]; then
+	## For Debian, *Ubuntu etc.
+	INSTALL="sudo apt-get -y install"
+else
+	## For RedHat, Fedora etc.
+	INSTALL="yum -y install"
+fi
 
-sudo apt-get -y install lib$MPI-dev libhdf5-$MPI-dev  			## OpenMPI version (TODO: try removing libhdf5-cpp-8)libhdf5-cpp-8
-#sudo apt-get -y install $MPI-bin		 ## TODO remove?
+$INSTALL autoconf chrpath debhelper git libtool swig wget 
 
-## for Ubuntu 15.04: fresh libctl 3.2.2 is in repository
-sudo apt-get install -y libctl-dev							
-## for Ubuntu 14.04, or older:  the version of `libctl-dev' in repository is too old, needs a fresh compile:
-#  wget http://ab-initio.mit.edu/libctl/libctl-3.2.1.tar.gz
-#  tar xzf libctl* && cd libctl-3.2.1/
-#  ./configure LIBS=-lm  &&  make  &&  sudo make install
-#  cd ..
+if [ -d /etc/apt ]; then 
+	$INSTALL autotools-dev          g++  		gfortran	guile-2.0-dev 	
+	$INSTALL h5utils 
+	$INSTALL imagemagick            libatlas-base-dev libfftw3-dev libgsl0-dev liblapack-dev
+	$INSTALL libharminv-dev pkg-config	## missing in RPM?
+	$INSTALL zlib1g-dev
+else
+	$INSTALL automake autoconf 	gcc-c++  	gcc-gfortran	guile-devel
+	echo "TODO: h5utils must be compiled on Fedora!"
+	$INSTALL ImageMagick		atlas-devel	fftw3-devel 	gsl-devel lapack-devel
+	echo "TODO: harminv must be (probably) compiled on Fedora!"
+	echo "TODO: the debian package of 'pkg-config' missing its counterpart on Fedora!"
+	$INSTALL zlib-devel
+fi
+
+if [ "$MPI" = "openmpi" ] || [ "$MPI" = "mpich" ] || [ "$MPI" = "mpich2" ] ; then
+	if [ "$PKGTYPE" = "deb" ]; then
+		$INSTALL lib$MPI-dev libhdf5-$MPI-dev  			
+	else
+		$INSTALL $MPI-devel hdf5-$MPI-devel
+	fi
+fi
+
+
+##   for Ubuntu 15.04: fresh libctl 3.2.2 is in repository and we can use it
+# $INSTALL libctl-dev							
+## for Ubuntu <= 14.04, or other distros:  the version of `libctl-dev' in repository is too old, needs a fresh compile:
+wget http://ab-initio.mit.edu/libctl/libctl-3.2.1.tar.gz
+tar xzf libctl* && cd libctl-3.2.1/
+./configure LIBS=-lm  &&  make  &&  sudo make install
+cd ..
 
 ## --- MEEP (now fresh from github!) --------------------------------------------
 export CFLAGS=" -fPIC"; export CXXFLAGS=" -fPIC"; export FFLAGS=" -fPIC"  ## Position Independent Code, needed on 64-bit
-export CPPFLAGS="-I/usr/include/hdf5/$MPI"
-export LDFLAGS="-L/usr/lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH)/hdf5/$MPI"
+if [ "$PKGTYPE" = "deb" ]; then
+	export CPPFLAGS="-I/usr/include/hdf5/$MPI"
+	export LDFLAGS="-L/usr/lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH)/hdf5/$MPI"
+else
+	echo "TODO: when HDF5 libraries are available for Fedora, include them as above"
+	PATH=$PATH:/usr/local/bin
+fi
 
 ## Note: If the git version was unavailable, use the failsafe alternative below
 #if [ ! -d "meep" ]; then git clone https://github.com/filipdominec/meep; fi   ## FD's branch, see github
@@ -49,7 +79,11 @@ cd ..
 
 ## --- PYTHON-MEEP ------------------------------------------------------------
 ## Install python-meep dependencies and SWIG
-sudo apt-get install python-dev python-numpy python-scipy -y
+if [ -d /etc/apt ]; then
+	$INSTALL python-dev python-numpy python-scipy python-matplotlib
+else
+	$INSTALL python-devel numpy scipy matplotlib
+fi
 
 ## Get the latest source from green block at https://launchpad.net/python-meep/1.4
 if [ ! -d "python-meep" ]; then
@@ -64,5 +98,10 @@ pm_opt=`echo $meep_opt | sed 's/--with//g'`
 sed -i -e 's:/usr/lib:/usr/local/lib:g' -e 's:/usr/include:/usr/local/include:g' ./setup${pm_opt}.py
 sed -i -e '/custom.hpp/ a export LD_RUN_PATH=\/usr\/local\/lib' make${pm_opt}
 sed -i -e 's/#global/global/g' -e 's/#DISABLE/DISABLE/g' -e 's/\t/    /g'  meep-site-init.py
+if [ ! -d /etc/apt/ ]; then 
+	## Fedora22 complained about "Unknown SWIG preprocessor directive" if the comment was left 
+	sed -i -e '/initialisations/d' meep-site-init.py
+fi
+
 sudo ./make${pm_opt} -I/usr/local/include -L/usr/local/lib
 
