88fef5a7950ce1002578530013b750c6912c0780
Filip Dominec <filip.dominec@gmail.com>
2015-09-18 15:39:06 +0200

    tested on 10.04, fixed problems

--- python-meep-install.sh
+++ python-meep-install.sh
@@ -1,4 +1,4 @@
-j!/bin/bash
+#!/bin/bash
 ## This is a compilation procedure that worked for me to setup the python-meep 
 ## with some utilities on a Debian-based system.
 
@@ -6,46 +6,51 @@ j!/bin/bash
 MPI="openmpi"
 #MPI="mpich"
 #MPI="mpich2"
-#MPI="serial"		## i.e. no multiprocessing used
+#MPI="serial"        ## i.e. no multiprocessing used
 
 ## --- Preparation and build dependencies -------------------------------------
 if [ "$MPI" = "openmpi" ] || [ "$MPI" = "mpich" ] || [ "$MPI" = "mpich2" ] ; then meep_opt="--with-mpi"; fi
 
 ## Switch installation commands between Debian-based systems and RPM-based systems
 if [ -d /etc/apt ]; then
-	INSTALL="sudo apt-get -y install"
+    INSTALL="sudo apt-get -y install"
 else
-	INSTALL="yum -y install"
+    INSTALL="yum -y install"
 fi
 
-$INSTALL autoconf chrpath debhelper git libtool swig wget 
+$INSTALL autoconf chrpath debhelper libtool swig wget 
+$INSTALL git          ## on newer linux versions
+$INSTALL git-core     ## for old ubuntu 10.04
 
 if [ -d /etc/apt ]; then 
-	$INSTALL autotools-dev          g++  		gfortran	guile-2.0-dev 	
-	$INSTALL h5utils 
-	$INSTALL imagemagick            libatlas-base-dev libfftw3-dev libgsl0-dev liblapack-dev
-	$INSTALL libharminv-dev pkg-config	## these are missing in RPM?
-	$INSTALL zlib1g-dev
+    $INSTALL autotools-dev          g++    gfortran         
+    $INSTALL h5utils 
+    $INSTALL imagemagick            libatlas-base-dev libfftw3-dev libgsl0-dev liblapack-dev
+    $INSTALL libharminv-dev pkg-config  ## these are missing in RPM?
+    $INSTALL zlib1g-dev
+    $INSTALL guile-2.0-dev   ## for newer linux versions
+    $INSTALL guile-1.8-dev   ## for old ubuntu 10.04
 else
-	$INSTALL automake autoconf 	gcc-c++  	gcc-gfortran	guile-devel
-	echo "TODO: h5utils must be compiled on Fedora!"
-	$INSTALL ImageMagick		atlas-devel	fftw3-devel 	gsl-devel lapack-devel
-	echo "TODO: harminv must be (probably) compiled on Fedora!"
-	echo "TODO: the debian package of 'pkg-config' missing its counterpart on Fedora!"
-	$INSTALL zlib-devel
+    $INSTALL automake autoconf     gcc-c++      gcc-gfortran    
+    echo "TODO: h5utils must be compiled on Fedora!"
+    $INSTALL ImageMagick        atlas-devel    fftw3-devel     gsl-devel lapack-devel
+    echo "TODO: harminv must be (probably) compiled on Fedora!"
+    echo "TODO: the debian package of 'pkg-config' missing its counterpart on Fedora!"
+    $INSTALL zlib-devel
+    $INSTALL guile-devel
 fi
 
 if [ "$MPI" = "openmpi" ] || [ "$MPI" = "mpich" ] || [ "$MPI" = "mpich2" ] ; then
-	if [ "$PKGTYPE" = "deb" ]; then
-		$INSTALL lib$MPI-dev libhdf5-$MPI-dev  			
-	else
-		$INSTALL $MPI-devel hdf5-$MPI-devel
-	fi
+    if [ "$PKGTYPE" = "deb" ]; then
+        $INSTALL lib$MPI-dev libhdf5-$MPI-dev              
+    else
+        $INSTALL $MPI-devel hdf5-$MPI-devel
+    fi
 fi
 
 
 ##   for Ubuntu 15.04: fresh libctl 3.2.2 is in repository and we can use it
-# $INSTALL libctl-dev							
+# $INSTALL libctl-dev                            
 ## for Ubuntu <= 14.04, or other distros:  the version of `libctl-dev' in repository is too old, needs a fresh compile:
 wget http://ab-initio.mit.edu/libctl/libctl-3.2.1.tar.gz
 tar xzf libctl* && cd libctl-3.2.1/
@@ -55,16 +60,16 @@ cd ..
 ## --- MEEP (now fresh from github!) --------------------------------------------
 export CFLAGS=" -fPIC"; export CXXFLAGS=" -fPIC"; export FFLAGS=" -fPIC"  ## Position Independent Code, needed on 64-bit
 if [ "$PKGTYPE" = "deb" ]; then
-	export CPPFLAGS="-I/usr/include/hdf5/$MPI"
-	export LDFLAGS="-L/usr/lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH)/hdf5/$MPI"
+    export CPPFLAGS="-I/usr/include/hdf5/$MPI"
+    export LDFLAGS="-L/usr/lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH)/hdf5/$MPI"
 else
-	echo "TODO: when HDF5 libraries are available for Fedora, include them as above"
-	PATH=$PATH:/usr/local/bin
+    echo "TODO: when HDF5 libraries are available for Fedora, include them as above"
+    PATH=$PATH:/usr/local/bin
 fi
 
 ## Note: If the git version was unavailable, use the failsafe alternative below
 if [ ! -d "meep" ]; then git clone https://github.com/filipdominec/meep; fi   ## FD's branch, see github
-#if [ ! -d "meep" ]; then git clone https://github.com/stevengj/meep; fi	  ## official branch
+#if [ ! -d "meep" ]; then git clone https://github.com/stevengj/meep; fi      ## official branch
 cd meep/
 ./autogen.sh $meep_opt --enable-maintainer-mode --enable-shared --prefix=/usr/local  # exits with 1 ?
 make  &&  sudo make install
@@ -79,15 +84,15 @@ cd ..
 ## --- PYTHON-MEEP ------------------------------------------------------------
 ## Install python-meep dependencies and SWIG
 if [ -d /etc/apt ]; then
-	$INSTALL python-dev python-numpy python-scipy python-matplotlib
+    $INSTALL python-dev python-numpy python-scipy python-matplotlib
 else
-	$INSTALL python-devel numpy scipy matplotlib
+    $INSTALL python-devel numpy scipy matplotlib
 fi
 
 ## Get the latest source from green block at https://launchpad.net/python-meep/1.4
 if [ ! -d "python-meep" ]; then
-	wget https://launchpad.net/python-meep/1.4/1.4/+download/python-meep-1.4.2.tar
-	tar xf python-meep-1.4.2.tar
+    wget https://launchpad.net/python-meep/1.4/1.4/+download/python-meep-1.4.2.tar
+    tar xf python-meep-1.4.2.tar
 fi
 
 ## If libmeep*.so was installed to /usr/local/lib, this scipt has to edit the installation scripts (aside
@@ -98,8 +103,8 @@ sed -i -e 's:/usr/lib:/usr/local/lib:g' -e 's:/usr/include:/usr/local/include:g'
 sed -i -e '/custom.hpp/ a export LD_RUN_PATH=\/usr\/local\/lib' make${pm_opt}
 sed -i -e 's/#global/global/g' -e 's/#DISABLE/DISABLE/g' -e 's/\t/    /g'  meep-site-init.py
 if [ ! -d /etc/apt/ ]; then 
-	## Fedora22 complained about "Unknown SWIG preprocessor directive" if the comment was left 
-	sed -i -e '/initialisations/d' meep-site-init.py
+    ## Fedora22 complained about "Unknown SWIG preprocessor directive" if the comment was left 
+    sed -i -e '/initialisations/d' meep-site-init.py
 fi
 
 sudo ./make${pm_opt} -I/usr/local/include -L/usr/local/lib
