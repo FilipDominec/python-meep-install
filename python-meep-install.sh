3bd2507515636276acad9cc2a7a49e3b16e23668
Filip Dominec <filip.dominec@gmail.com>
2015-09-19 11:28:19 +0200

    fixed libmeep-missing error by adding LD_RUN_PATH

--- python-meep-install.sh
+++ python-meep-install.sh
@@ -14,8 +14,10 @@ if [ "$MPI" = "openmpi" ] || [ "$MPI" = "mpich" ] || [ "$MPI" = "mpich2" ] ; the
 ## Switch installation commands between Debian-based systems and RPM-based systems
 if [ -d /etc/apt ]; then
     INSTALL="sudo apt-get -y install"
+    sudo apt-get update
 else
     INSTALL="yum -y install"
+    sudo yum update
 fi
 
 $INSTALL autoconf chrpath debhelper libtool swig wget 
@@ -52,18 +54,22 @@ fi
 ##   for Ubuntu 15.04: fresh libctl 3.2.2 is in repository and we can use it
 # $INSTALL libctl-dev                            
 ## for Ubuntu <= 14.04, or other distros:  the version of `libctl-dev' in repository is too old, needs a fresh compile:
-wget http://ab-initio.mit.edu/libctl/libctl-3.2.1.tar.gz
-tar xzf libctl* && cd libctl-3.2.1/
+if [ ! -d libctl-3.2.1/ ]; then
+	wget http://ab-initio.mit.edu/libctl/libctl-3.2.1.tar.gz
+	tar xzf libctl*
+fi
+cd libctl-3.2.1/
 ./configure LIBS=-lm  &&  make  &&  sudo make install
 cd ..
 
 ## --- MEEP (now fresh from github!) --------------------------------------------
 export CFLAGS=" -fPIC"; export CXXFLAGS=" -fPIC"; export FFLAGS=" -fPIC"  ## Position Independent Code, needed on 64-bit
 if [ -d /etc/apt ]; then
-    export CPPFLAGS="-I/usr/include/hdf5/$MPI"
+    export CPPFLAGS="-I/usr/local/include -I/usr/include/hdf5/$MPI"
     export LDFLAGS="-L/usr/lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH)/hdf5/$MPI"
+    export LD_RUN_PATH="/usr/local/lib"
 else
-    echo "TODO: when HDF5 libraries are available for Fedora, include them as above"
+    echo "TODO: enable HDF5 libraries for RPM distros"
     PATH=$PATH:/usr/local/bin
 fi
 
@@ -91,7 +97,9 @@ fi
 
 ## Get the latest source from green block at https://launchpad.net/python-meep/1.4
 if [ ! -d "python-meep" ]; then
-    wget https://launchpad.net/python-meep/1.4/1.4/+download/python-meep-1.4.2.tar
+    if [ ! -f "python-meep-1.4.2.tar" ]; then
+	    wget https://launchpad.net/python-meep/1.4/1.4/+download/python-meep-1.4.2.tar
+    fi
     tar xf python-meep-1.4.2.tar
 fi
 
