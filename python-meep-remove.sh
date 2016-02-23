5348f42d68d1b44f4d5996a637c424334c9d2758
Filip Dominec <filip.dominec@gmail.com>
2016-02-23 12:29:28 +0100

    enable uninstalling the serial meep version

--- python-meep-remove.sh
+++ python-meep-remove.sh
@@ -1,19 +1,22 @@
-# == Uninstall procedure ==
+# Set to _mpi if you used a mpi version, leave it empty if not
+MPI=_mpi
+
+# == Uninstall procedure == 
 echo Removing locally compiled meep
 rm /usr/local/bin/meep-mpi
 rm /usr/local/include/meep.hpp
 rm /usr/local/include/meep/mympi.hpp
 rm /usr/local/include/meep/vec.hpp
-rm /usr/local/lib/libmeep_mpi.a
-rm /usr/local/lib/libmeep_mpi.la
-rm /usr/local/lib/libmeep_mpi.so*
-rm /usr/local/lib/pkgconfig/meep_mpi.pc
+rm /usr/local/lib/libmeep${MPI}.a
+rm /usr/local/lib/libmeep${MPI}.la
+rm /usr/local/lib/libmeep${MPI}.so*
+rm /usr/local/lib/pkgconfig/meep${MPI}.pc
 rm /usr/local/share/meep/casimir.scm
 rm /usr/local/share/meep/meep-enums.scm
 rm /usr/local/share/meep/meep.scm
 echo 'Removing locally compiled python-meep (works for python 2.7)...'
-rm /usr/local/lib/python2.7/dist-packages/_meep_mpi.so
-rm /usr/local/lib/python2.7/dist-packages/meep_mpi.py
-rm /usr/local/lib/python2.7/dist-packages/meep_mpi.pyc
+rm /usr/local/lib/python2.7/dist-packages/_meep${MPI}.so
+rm /usr/local/lib/python2.7/dist-packages/meep${MPI}.py
+rm /usr/local/lib/python2.7/dist-packages/meep${MPI}.pyc
 rm /usr/local/lib/python2.7/dist-packages/python_meep-1.4.egg-info
 echo done.
